27e6092f29b3a5f7202ae3a881ff02731d22121e
JamesPiggott <jjhpiggott@gmail.com>
2021-03-14 12:44:42 +0100

    Started with nmap scanner in Python

---
+++ Penetration Testing/2 - Scanning/nmap_scanner/python/Scanner.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import nmap
+
+scanner = nmap.PortScanner()
+
+print("Welcome, this is a simple nmap automation tool")
+print("<----------------------------------------------------->")
+
+ip_addr = input("Please enter the IP address you want to scan: ")
+print("The IP you entered is: ", ip_addr)
+type(ip_addr)
+
+
+
+
